da7c03227118dc9a1477f1019753cae2e5b805f6
Mellabelli <36308043+Mellabelli@users.noreply.github.com>
2018-03-21 21:04:14 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdlib>
+#include <stdio.h>
+#include <conio.h>
+
+using namespace std;
+float x;
+float y;
+char wybor;
+
+int main()
+{
+    for(;;)
+    {
+
+    cout << "Podaj pierwsza liczbe" << endl;
+    cin >> x;
+    cout << "Podaj druga liczbe" << endl;
+    cin >> y;
+
+    cout << " \n Kalkulator menu: \n _______ \n \n 1. Dodawanie \n 2. Odejmowanie \n 3. Mnozenie \n 4. Dzielenie \n 5. Wyjscie"  <<endl;
+    cout << "Twoj wybor: ";
+    cout << endl;
+    wybor = getch();
+    switch(wybor)
+    {
+    case '1':
+        cout << "Suma: " <<  x+y << endl;
+        break;
+    case '2':
+        cout << "Ró¿nica: " << x-y <<endl;
+        break;
+    case '3':
+       cout << "Iloczyn: " << x*y <<endl;
+        break;
+    case '4':
+        if (y==0) cout << "Nie dzielimy przez 0!" <<endl;
+        cout << "Iloraz: " <<x/y <<endl;
+        break;
+    case '5':
+        exit(0);
+        break;
+
+    default: cout << "Takiej opcji nie ma w menu!";
+    }
+    getchar(); getchar();
+    system("cls");
+    }
+    return 0;
+}
